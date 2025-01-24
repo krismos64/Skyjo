1f1bb24eabeac410c07598c4005e0d5051dfd59d
krismos64 <c.mostefaoui@yahoo.fr>
2025-01-24 19:43:28 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
-      '/socket.io': {
-        target: 'ws://localhost:3000',
-        ws: true
+      "/socket.io": {
+        target: "ws://localhost:3000",
+        ws: true,
       },
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+    rollupOptions: {
+      external: [
+        // Add the module Rollup failed to resolve here
+        "/assets/index-15559f11.js",
+      ],
+    },
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
